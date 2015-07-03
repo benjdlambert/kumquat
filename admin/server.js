33dd525fd23eed92e352c898fb05ddde1043a6ec
alcockp01 <paul.alcock@bbc.co.uk>
2015-07-03 01:09:28 +0100

    Started work on the front end

--- admin/server.js
+++ admin/server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     exphbs  = require('express-handlebars'),
     app = express(),
     routes = require('./app/routes');
+    hbs = require('hbs'),
 
 app.engine('handlebars', exphbs({
         defaultLayout: 'main',
@@ -9,7 +10,10 @@ app.engine('handlebars', exphbs({
 );
 
 app.set('view engine', 'handlebars');
+var temp = __dirname + '/static';
 
+hbs.registerPartials(__dirname + '/app/views/partials');
+app.use('/static/', express.static(__dirname + '/static'));
 routes(app);
 
 app.listen(process.env.PORT || 8080);
