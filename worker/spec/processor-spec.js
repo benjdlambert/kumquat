af0510c96b8dae046c6e5ddee84a4394c5042b66
blam <benjamin.lambert@bbc.co.uk>
2015-07-23 20:06:37 +0100

    Starting to add some tests for processor

--- worker/spec/processor-spec.js
+++ worker/spec/processor-spec.js
@@ -1,16 +1,38 @@
-describe('Processor', function() {
-    'use strict';
+ describe('Processor', function() {
 
-    var messageRetriever,
-        Promise = require('bluebird'),
+    var Promise = require('bluebird'),
         mongoose = require('mongoose'),
-        processor;
-
-    beforeEach(function() {
-        messageRetriever = require('../lib/message-retriever');
+        mockModelHelper = require('./mock-model-helper'),
+        mongod = require('./mongo-helper'),
+        testManager = require('./test-suites/test-manager'),
         processor = require('../lib/processor');
+        Test = require('../../common/models/test');
 
+    beforeEach(function(done) {
         Promise.promisifyAll(mongoose);
+        mongod.setup()
+            .then(done);
+    });
+
+    afterEach(function(done) {
+        mongod.teardown()
+            .then(done)
+    })
+
+    xit('should create a baseline for a test if it does not exist already', function(done) {
+        Promise
+            .all([
+                mockModelHelper.createVistFlowStep('same'),
+                mockModelHelper.createScreenshotAssertion()
+            ])
+            .then(mockModelHelper.createTestWithFlow)
+            .then(function(test) {
+                this.testId = test.id;
+                return processor.process(this.id);
+            })
+            .then(function() {
+                done();
+            });
     });
 
     it('should create a test run for a message');
@@ -18,5 +40,4 @@ describe('Processor', function() {
     it('should not fail if two sites are not different');
     it('should compare components and fail if different');
     it('should compare components and not fail if not different');
-
 });
